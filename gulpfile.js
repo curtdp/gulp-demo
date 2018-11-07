bdc556f4012fd5c5f3e747f1fa1068a3b76808f8
Igor Oliinyk <igor.oleynikdp@gmail.com>
2018-11-07 19:32:32 +0200

    добавили browsersync

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,14 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
+const browserSync = require('browser-sync').create();
 
 sass.compiler = require('node-sass');
 
 function sassTask() {
   return gulp.src('./src/sass/**/*.scss')
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./build/css'));
+    .pipe(gulp.dest('./build/css'))
+    .pipe(browserSync.stream());
 }
 
 gulp.task('sass', sassTask);
@@ -14,3 +16,14 @@ gulp.task('sass', sassTask);
 gulp.task('sass:watch', function () {
   gulp.watch(['./src/sass/**/*.scss'], sassTask);
 });
+
+// Static server
+gulp.task('dev', function() {
+  browserSync.init({
+    server: {
+      baseDir: "./"
+    }
+  });
+  gulp.watch(['./src/sass/**/*.scss'], sassTask);
+  gulp.watch(['./*.html']).on('change', browserSync.reload);
+})
